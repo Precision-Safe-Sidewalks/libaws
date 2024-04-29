fa45b1113181db57b132a0b292220482e0cb1a9e
Alex Curley <=>
2024-04-29 09:07:23 -0400

    Add endpoint URL to lambda invocation

--- src/lambda.rs
+++ src/lambda.rs
@@ -7,29 +7,41 @@ use tokio::runtime::Runtime;
 
 /// Invoke a Lambda function
 #[pyfunction]
-pub fn invoke_lambda_function(function_name: String, payload: String) -> bool {
+pub fn invoke_lambda_function(
+    function_name: String,
+    payload: String,
+    endpoint_url: String,
+) -> bool {
     Runtime::new()
         .unwrap()
-        .block_on(async { ainvoke_lambda_function(function_name, payload).await })
+        .block_on(async { ainvoke_lambda_function(function_name, payload, endpoint_url).await })
 }
 
 /// Invoke a Lambda function asynchronously
-async fn ainvoke_lambda_function(function_name: String, payload: String) -> bool {
-    let client = get_client().await;
+async fn ainvoke_lambda_function(
+    function_name: String,
+    payload: String,
+    endpoint_url: String,
+) -> bool {
+    let client = get_client(endpoint_url).await;
 
     client
         .invoke()
         .function_name(function_name)
         .payload(Blob::new(payload))
-        .invocation_type(InvocationType::Event)
+        .invocation_type(InvocationType::RequestResponse)
         .send()
         .await
         .is_ok()
 }
 
 /// Get the Lambda client using environment variables
-async fn get_client() -> Client {
+async fn get_client(endpoint_url: String) -> Client {
     let region = RegionProviderChain::default_provider().or_else("us-east-1");
-    let config = aws_config::from_env().region(region).load().await;
+    let config = aws_config::from_env()
+        .endpoint_url(endpoint_url)
+        .region(region)
+        .load()
+        .await;
     Client::new(&config)
 }
